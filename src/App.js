cad51bfbb85a16f709db987d7a7b9b8267ce9a80
jpcavalheiro <joaopaulo1060@hotmail.com>
2021-05-31 19:11:13 -0300

    Iniciando desafio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import GlobalStyle from './GlobalStyles/global'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <GlobalStyle/>
+    </div>
+  );
+}
+
+export default App;
