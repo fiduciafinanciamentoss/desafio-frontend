7eff03ab96f3f9e15428cc6687d0a76ecc9d3210
Guilherme Tosta <guilherme_tosta@hotmail.com>
2021-06-07 13:08:01 -0300

    Loading e Busca Att

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,16 +1,19 @@
 import "./Pokedex.css";
 import { useState, useEffect } from "react";
-import React from 'react'
-import Modal from './components/Modal/modal'
+import React from "react";
+import Modal from "./components/Modal/modal";
+import Loading from "./components/Loading/loading";
 
 function Pokedex() {
   const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
+  const [filteredPokemons, setFilteredPokemons] = useState([]);
+  const [searchTextInput, setSearchTextInput] = useState();
   const [selectedPokemon, setSelectedPokemon] = useState();
 
   useEffect(() => {
     const getPokemon = (id) => fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const pokemonIds = Array.from({ length: 25 }, (empty, index) => index + 1);
+    const pokemonIds = Array.from({ length: 250 }, (empty, index) => index + 1);
     const allPromises = pokemonIds.map((number) => {
       return getPokemon(number).then((response) => response.json());
     });
@@ -21,21 +24,36 @@ function Pokedex() {
       setPokemons(pokemons);
     });
   }, []);
-    
 
   if (loading) {
-    return (
-      <h1 style={{ color: "white", fontSize: "89px", justifySelf: "center" }}>
-        Carregando...
-      </h1>
-    );
+    return <Loading />;
   }
 
-  console.log(selectedPokemon)
+  function armazenarFiltro() {
+    const filteredPokemons = pokemons.filter((pokemon) => {
+      return pokemon.name.includes(searchTextInput.toLowerCase());
+    });
+    setFilteredPokemons(filteredPokemons);
+  }
+
+  function buscaPoke() {
+    const filteredPokemons = pokemons.filter((pokemon) => {
+      return pokemon.name.includes(searchTextInput.toLowerCase());
+    });
+    setFilteredPokemons(filteredPokemons);
+  }
+
+  const pokemonsToRender =
+    filteredPokemons.length > 0 ? filteredPokemons : pokemons;
 
   return (
     <div className="App">
-      {selectedPokemon && <Modal pokemon={selectedPokemon} onClose={() => setSelectedPokemon(null)} />}
+      {selectedPokemon && (
+        <Modal
+          pokemon={selectedPokemon}
+          onClose={() => setSelectedPokemon(null)}
+        />
+      )}
 
       <header className="App-header">
         <img
@@ -52,25 +70,33 @@ function Pokedex() {
         </div>
 
         <div className="search-container">
-          <input type="text" placeholder="Buscar..." />
-          <button>🔍</button>
+          <input
+            onChange={(event) => {
+              const value = event.target.value;
+              setSearchTextInput(value);
+            }}
+            type="text"
+            placeholder="Buscar..."
+          />
+          <button onClick={buscaPoke}>🔍</button>
         </div>
       </header>
       <div className="App-body">
         <div className="cards-container">
-          {pokemons.map((pokemon) => {
+          {pokemonsToRender.map((pokemon) => {
             return (
               <div key={pokemon.id} className="card">
                 <h2 className="card-title">{pokemon.name}</h2>
-            
 
-                <div className="card-img-container" onClick={() => setSelectedPokemon(pokemon)}>
+                <div
+                  className="card-img-container"
+                  onClick={() => setSelectedPokemon(pokemon)}
+                >
                   <img
                     loading="lazy"
                     src={`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`}
                     className="card-img"
                     alt={pokemon.name}
-
                   />
                 </div>
 
@@ -78,7 +104,7 @@ function Pokedex() {
                   {pokemon.types.map((pokeType) => {
                     return (
                       <div key={pokeType.type.name} className="card-type">
-                      <h4> {pokeType.type.name} </h4>
+                        <h4> {pokeType.type.name} </h4>
                       </div>
                     );
                   })}
---
+++ src/components/Loading/loading.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import './loading.css'
+
+function Loading(){
+  return(
+<div className="tela-loading">
+      <img
+      src="/images/loading.gif"
+      className="img-loading"
+      alt="Pokebola"
+      />
+</div>
+  )
+};
+export default Loading;
